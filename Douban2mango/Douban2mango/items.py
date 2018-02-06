258ae5cca59f48ab23365c3393c669dfbf60f77b
mr_stone <1436882538@qq.com>
2018-02-06 15:11:50 +0800

    增加 数据入库mango

---
+++ Douban2mango/Douban2mango/items.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+# Define here the models for your scraped items
+#
+# See documentation in:
+# http://doc.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class Douban2MangoItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    title = scrapy.Field()
+    # 电影评分
+    score = scrapy.Field()
+    # 电影信息
+    content = scrapy.Field()
+    # 简介
+    info = scrapy.Field()
---
+++ Douban2mango/Douban2mango/spiders/douban.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+import scrapy
+from Douban2mango.items import Douban2MangoItem
+
+
+class DoubanSpider(scrapy.Spider):
+    name = "douban"
+    allowed_domains = ["movie.douban.com"]
+    start = 0
+    url = 'https://movie.douban.com/top250?start='
+    end = '&filter='
+    start_urls = [url + str(start) + end]
+
+    def parse(self, response):
+
+        item = Douban2MangoItem()
+
+        movies = response.xpath("//div[@class=\'info\']")
+
+        for each in movies:
+            title = each.xpath('div[@class="hd"]/a/span[@class="title"]/text()').extract()
+            content = each.xpath('div[@class="bd"]/p/text()').extract()
+            score = each.xpath('div[@class="bd"]/div[@class="star"]/span[@class="rating_num"]/text()').extract()
+            info = each.xpath('div[@class="bd"]/p[@class="quote"]/span/text()').extract()
+
+            item['title'] = title[0]
+            # 以;作为分隔，将content列表里所有元素合并成一个新的字符串
+            item['content'] = ';'.join(content)
+            item['score'] = score[0]
+            item['info'] = info[0]
+            # 提交item
+
+            yield item
+
+        if self.start <= 225:
+            self.start += 25
+            yield scrapy.Request(self.url + str(self.start) + self.end, callback=self.parse)
